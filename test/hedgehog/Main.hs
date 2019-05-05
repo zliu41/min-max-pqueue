e5699b8b991087be91ba12634916c74fb3fb2641
Ziyang Liu <free@cofree.io>
2019-05-04 19:15:47 -0700

    Tests for MinMaxQueue

--- test/hedgehog/Main.hs
+++ test/hedgehog/Main.hs
@@ -6,10 +6,11 @@ import           System.Exit (exitFailure)
 import           System.IO (hSetEncoding, stdout, stderr)
 
 import qualified IntMinMaxQueueSpec
+import qualified MinMaxQueueSpec
 
 main :: IO ()
 main = do
   hSetEncoding stdout utf8
   hSetEncoding stderr utf8
-  passed <- IntMinMaxQueueSpec.tests
-  unless passed exitFailure
+  passed <- sequenceA [MinMaxQueueSpec.tests, IntMinMaxQueueSpec.tests]
+  unless (and passed) exitFailure
