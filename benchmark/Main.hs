0dcc54658554559a30233bf27f4fafe8712418f0
Ziyang Liu <free@cofree.io>
2019-05-04 17:46:43 -0700

    Benchmark for MinMaxQueue

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -5,17 +5,27 @@ import           Data.List (sort, sortBy, unfoldr)
 import           System.Random
 
 import qualified Data.IntMinMaxQueue as IPQ
+import qualified Data.MinMaxQueue as PQ
 
 bench :: [Criterion.Benchmark]
 bench =
   [ Criterion.bench "intpq-asc-min" $
       Criterion.nf (unfoldr IPQ.pollMin) (IPQ.fromListWith id ascElems)
-  , Criterion.bench "intpq-asc-max" $
-      Criterion.nf (unfoldr IPQ.pollMax) (IPQ.fromListWith id ascElems)
+  , Criterion.bench "intpq-desc-max" $
+      Criterion.nf (unfoldr IPQ.pollMax) (IPQ.fromListWith id descElems)
   , Criterion.bench "intpq-rand-min" $
       Criterion.nf (unfoldr IPQ.pollMin) (IPQ.fromListWith id randomElems)
   , Criterion.bench "intpq-rand-max" $
       Criterion.nf (unfoldr IPQ.pollMax) (IPQ.fromListWith id randomElems)
+
+  , Criterion.bench "pq-asc-min" $
+      Criterion.nf (unfoldr PQ.pollMin) (PQ.fromListWith id ascElems)
+  , Criterion.bench "pq-desc-max" $
+      Criterion.nf (unfoldr PQ.pollMax) (PQ.fromListWith id descElems)
+  , Criterion.bench "pq-rand-min" $
+      Criterion.nf (unfoldr PQ.pollMin) (PQ.fromListWith id randomElems)
+  , Criterion.bench "pq-rand-max" $
+      Criterion.nf (unfoldr PQ.pollMax) (PQ.fromListWith id randomElems)
   ]
 
 main :: IO ()
