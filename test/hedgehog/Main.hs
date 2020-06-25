c7468bba82fbc861bb70522cae9b0c3e8149e707
Ziyang Liu <unsafeFixIO@gmail.com>
2020-06-24 22:58:23 -0700

    Remove hSetEncoding in hedgehog test

--- test/hedgehog/Main.hs
+++ test/hedgehog/Main.hs
@@ -1,7 +1,6 @@
 module Main (main) where
 
 import           Control.Monad (unless)
-import           GHC.IO.Encoding (utf16le)
 import           System.Exit (exitFailure)
 import           System.IO (hSetEncoding, stdout, stderr)
 
@@ -10,7 +9,5 @@ import qualified MinMaxQueueSpec
 
 main :: IO ()
 main = do
-  hSetEncoding stdout utf16le
-  hSetEncoding stderr utf16le
   passed <- sequenceA [MinMaxQueueSpec.tests, IntMinMaxQueueSpec.tests]
   unless (and passed) exitFailure
